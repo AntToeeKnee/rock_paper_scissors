52aa3ef2d6cb3587a8b089b120a7ec92067b1f84
AntToeeKnee <124924118+AntToeeKnee@users.noreply.github.com>
2023-09-21 21:40:44 -0700

    Finished Game

--- javascript.js
+++ javascript.js
@@ -1,21 +1,52 @@
 //Number to be used in getComputerChoice
-const computerNumber = getRandomInt(3);
-console.log(getComputerChoice());
+let numberGiven = getRandomInt(3);
+let computerSelectionName = getComputerChoice(numberGiven);
+console.log(computerSelectionName);
 
-function getComputerChoice() {
-    if(computerNumber == 0) {
+//calls function and asks player for choice
+let playerSelection = getPlayerChoice();
+console.log(playerSelection);
+
+playRound(computerSelectionName, playerSelection);
+
+function playRound(computerSelectionName, playerSelection) {
+    if(computerSelectionName == playerSelection) {
+        console.log("Tie!");
+    }
+    else if(computerSelectionName == "Rock" && playerSelection == "Scissors" || computerSelectionName == "Paper" && playerSelection == "Rock" || computerSelectionName == "Scissors" && playerSelection == "Paper") {
+        console.log("You lose")
+    }
+    else {
+        console.log("You win!")
+    }
+}
+
+function getComputerChoice(numberGivenMethod) {
+    if(numberGivenMethod == 0) {
         return "Rock";
     }
-    else if(computerNumber == 1) {
+    else if(numberGivenMethod == 1) {
         return "Paper";
     }
-    else if(computerNumber == 2) {
+    else if(numberGivenMethod == 2) {
         return "Scissors";
     }
 }
 
-function playRound( getComputerChoice, ) {
-
+function getPlayerChoice() {
+    let choiceHolder = prompt("Rock, Paper, or Scissors?");
+    if(choiceHolder.toLowerCase() == "rock") {
+        return "Rock";
+    }
+    else if(choiceHolder.toLowerCase() == "paper") {
+        return "Paper";
+    }
+    else if(choiceHolder.toLowerCase() == "scissors") {
+        return "Scissors";
+    }
+    else {
+        console.log("Not a valid choice")
+    }
 }
 
 function getRandomInt(max) {
